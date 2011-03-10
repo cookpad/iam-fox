3a0b5fa9acc97a66aeaa0054b1cd5bc67ef7a0ff
winebarrel <sgwr_dts@yahoo.co.jp>
2011-03-10 21:22:41 +0900

    etc

--- Resources/chrome/content/main.js
+++ Resources/chrome/content/main.js
@@ -42,7 +42,7 @@ function treeViews() {
 
 function openAccountDialog() {
   openDialog('chrome://iamfox/content/account-dialog.xul', 'account-dialog', 'modeless',
-             {prefs:Prefs, views:treeViews(), newIAMClient:newIAMClient});
+             {currentView:selectedView(), newIAMClient:newIAMClient});
 }
 
 function newIAMClient() {
