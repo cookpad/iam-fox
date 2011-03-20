750e5b4b9b0d554b10dd42271cf4605ce1a80965
winebarrel <sgwr_dts@yahoo.co.jp>
2011-03-20 16:09:16 +0900

    delete cert/group when delete user

--- Resources/chrome/content/user-tree-view.js
+++ Resources/chrome/content/user-tree-view.js
@@ -64,64 +64,6 @@ UserTreeView.prototype = {
     }
   },
 
-  deleteUser: function() {
-    var user = this.selectedRow();
-    var userName = user.UserName;
-
-    if (!confirm("Are you sure you want to delete '" + userName + " ' ?")) {
-      return;
-    }
-
-    protect(function() {
-      inProgress(function() {
-        var xhr = this.iamcli.query_or_die('ListAccessKeys', [['UserName', userName]]);
-
-        for each (var member in xhr.xml()..AccessKeyMetadata.member) {
-          var params = [['UserName', userName], ['AccessKeyId', member.AccessKeyId]];
-          this.iamcli.query_or_die('DeleteAccessKey', params);
-        }
-
-        xhr = this.iamcli.query_or_die('ListUserPolicies', [['UserName', userName]]);
-
-        for each (var member in xhr.xml()..PolicyNames.member) {
-          var params = [['UserName', userName], ['PolicyName', member]];
-          this.iamcli.query_or_die('DeleteUserPolicy', params);
-        }
-
-        this.iamcli.query_or_die('DeleteUser', [['UserName', userName]]);
-
-        this.deleteCurrentRow();
-        this.tree.invalidate();
-      }.bind(this));
-    }.bind(this));
-  },
-
-  openUserEditDialog: function() {
-    var user = this.selectedRow();
-    openDialog('chrome://iamfox/content/user-edit-dialog.xul', 'user-edit-dialog', 'chrome,modal',
-               {view:this, inProgress:inProgress, user:user});
-  },
-
-  openViewKeyDialog: function() {
-    var user = this.selectedRow();
-    openDialog('chrome://iamfox/content/user-view-key-dialog.xul', 'user-edit-dialog', 'chrome,modal',
-               {view:this, inProgress:inProgress, user:user});
-  },
-
-  selectByName: function(name) {
-    var rows = this.rows;
-
-    for (var i = 0; i < rows.length; i++) {
-      var user = rows[i];
-
-      if (user.UserName == name) {
-        this.selection.select(i);
-      }
-    }
-
-    this.rows
-  },
-
   openUserCertWindow: function(event) {
     var user = this.selectedRow();
     var userName = user.UserName;
@@ -145,6 +87,20 @@ UserTreeView.prototype = {
         for each (var member in xhr.xml()..AccessKeyMetadata.member) {
           var params = [['UserName', userName], ['AccessKeyId', member.AccessKeyId]];
           this.iamcli.query_or_die('DeleteAccessKey', params);
+        }
+
+        xhr = this.iamcli.query_or_die('ListGroupsForUser', [['UserName', userName]]);
+
+        for each (var member in xhr.xml()..Groups.member) {
+          var params = [['UserName', userName], ['GroupName', member.GroupName]];
+          this.iamcli.query_or_die('RemoveUserFromGroup', params);
+        }
+
+        xhr = this.iamcli.query_or_die('ListSigningCertificates', [['UserName', userName]]);
+
+        for each (var member in xhr.xml()..Certificates.member) {
+          var params = [['UserName', userName], ['CertificateId', member.CertificateId]];
+          this.iamcli.query_or_die('DeleteSigningCertificate', params);
         }
 
         xhr = this.iamcli.query_or_die('ListUserPolicies', [['UserName', userName]]);
