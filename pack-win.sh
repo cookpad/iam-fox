6050e9b7347868d19ab5a422d8e84c7121ce7589
winebarrel <sgwr_dts@yahoo.co.jp>
2011-03-20 17:16:10 +0900

    add instaler for win

---
+++ pack-win.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+VERSION=0.1.0
+SRC_DIR=iam-fox_win
+
+rm -rf $SRC_DIR *setup.exe
+mkdir $SRC_DIR
+cp -pr Resources $SRC_DIR
+cd $SRC_DIR
+rsync -rl /usr/local/xulrunner ./
+cd ..
+cygstart -w iam-fox.ci
+mv setup.exe IAMFox-${VERSION}-setup.exe
