882591fb65c11f135fe399cca46fec0393e1bd42
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2011-03-22 01:52:22 +0900

    etc

--- pack-osx.sh
+++ pack-osx.sh
@@ -4,3 +4,5 @@ APP=IAMFox.app
 rm -rf IAMFox.app *.dmg
 cp -pr IAMFox-src.app IAMFox.app
 cp -p /Library/Frameworks/XUL.framework/Versions/Current/xulrunner IAMFox.app/Contents/MacOS
+mkdir IAMFox.app/Contents/Frameworks
+rsync -rl /Library/Frameworks/XUL.framework IAMFox.app/Contents/Frameworks/
