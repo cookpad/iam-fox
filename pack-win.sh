3161e98a993d25cdaabee4d5c567c8c55b6e2f4a
winebarrel <sgwr_dts@yahoo.co.jp>
2011-03-21 22:21:33 +0900

    add shortcut icon

--- pack-win.sh
+++ pack-win.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-VERSION=0.1.0
+VERSION=0.1.1
 SRC_DIR=iam-fox_win
 
 rm -rf $SRC_DIR *setup.exe
@@ -7,6 +7,7 @@ cp -pr Resources $SRC_DIR
 cd $SRC_DIR
 cp /usr/local/xulrunner/xulrunner-stub.exe ./iam-fox.exe
 rsync -rl /usr/local/xulrunner ./
+cp ../iam-fox.ico .
 cd ..
 cygstart -w iam-fox.ci
 mv setup.exe IAMFox-${VERSION}-setup.exe
