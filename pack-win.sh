1d079a8177f2f749b2689db33a30a929b9c8833c
winebarrel <sgwr_dts@yahoo.co.jp>
2011-03-21 22:04:43 +0900

    change win packageing

--- pack-win.sh
+++ pack-win.sh
@@ -5,7 +5,8 @@ SRC_DIR=iam-fox_win
 rm -rf $SRC_DIR *setup.exe
 cp -pr Resources $SRC_DIR
 cd $SRC_DIR
+cp /usr/local/xulrunner/xulrunner-stub.exe ./iam-fox.exe
 rsync -rl /usr/local/xulrunner ./
 cd ..
-cygstart -w iam-fox.ci
-mv setup.exe IAMFox-${VERSION}-setup.exe
+#cygstart -w iam-fox.ci
+#mv setup.exe IAMFox-${VERSION}-setup.exe
