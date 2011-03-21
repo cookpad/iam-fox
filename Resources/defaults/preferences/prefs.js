bbb926c98c9bb6f00073a88217e4da7d9750dd1d
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2011-03-22 01:42:14 +0900

    remove window.close

--- Resources/defaults/preferences/prefs.js
+++ Resources/defaults/preferences/prefs.js
@@ -1,6 +1,5 @@
 pref("toolkit.defaultChromeURI", "chrome://iamfox/content/main.xul");
 pref("toolkit.defaultChromeFeatures", "chrome,centerscreen,width=800,height=600,resizable");
-pref("nglayout.debug.disable_xul_cache", true);
 /*
 pref("browser.dom.window.dump.enabled", true);
 pref("javascript.options.showInConsole", true);
